76405e22684c3d39d167fdfc62ff6e1f061e9b3a
ChrisBennett <cab9223@rit.edu>
2017-04-30 22:10:48 -0400

    Changed back some testing stuff

--- Source/AppClassControls.cpp
+++ Source/AppClassControls.cpp
@@ -91,11 +91,8 @@ void AppClass::ProcessKeyboard(void)
 	//TEST KILL BULLET USING T -- Will be triggered by touching enemy core
 	if ((sf::Keyboard::isKeyPressed(sf::Keyboard::T)) && player->killBullet->GetActiveBullet() == false) // Activate Kill Bullet
 	{
-		enemy->enemyBullet = new Bullet(enemy->GetPosition(), 3, 3);
-		enemy->enemyBullet->ChangePosition(vector3(0.0f, 0.0f, 0.0f), player->GetPlayerPosition());
-		enemy->Shoot();
-		//player->killBullet = new Bullet(playerPos, 1, 1);
-		//player->killBullet->SetActiveBullet(true);
+		player->killBullet = new Bullet(playerPos, 1, 1);
+		player->killBullet->SetActiveBullet(true);
 	}
 #pragma endregion
 
